e4d9a02dd477e8e69d9acf96e23da3ce1634f0fa
Diego Villacis <diegovillacis101@gmail.com>
2021-11-07 17:27:14 -0500

    Simple login with jwt

---
+++ authServer.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+app.use(express.json());
+
+// Refresh token could be better to store them in a DB or a Redis cache
+let refreshTokens = [];
+
+app.post("/login", (req, res) => {
+  //Authenticate user
+
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+
+  res.json({ accessToken, refreshToken });
+});
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+}
+
+app.listen(4000);
